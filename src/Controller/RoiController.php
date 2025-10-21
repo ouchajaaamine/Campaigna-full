db9b5c5efd06deab6151801f6aea094b16af853d
ouchajaaamine <ouchajaaamine@gmail.com>
2025-10-21 02:40:39 +0100

    adding roi controller and appFixtures to seed data

---
+++ src/Controller/RoiController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Controller;
+
+
+use App\Entity\Campaign;
+use App\Repository\CampaignRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Psr\Log\LoggerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpKernel\Attribute\AsController;
+
+#[AsController]
+class RoiController extends AbstractController
+{
+    public function __construct(
+        private EntityManagerInterface $entityManager,
+        private LoggerInterface $logger,
+        private CampaignRepository $campaignRepository
+    ) {}
+
+    public function __invoke(int $id)
+    {
+        $this->logger->info('Attempting to retrieve campaign with ID: ' . $id);
+        
+        $campaign = $this->entityManager->getRepository(Campaign::class)->find($id);
+        
+        if (!$campaign) {
+            $this->logger->warning('Campaign not found for ID: ' . $id);
+            return new JsonResponse(['message' => 'Campaign with ID ' . $id . ' not found.'], JsonResponse::HTTP_NOT_FOUND);
+        }
+
+
+        $totalSpent = $campaign->getBudget();
+        
+        $totalRevenue = $this->campaignRepository->getTotalRevenueByCampaignId($id);
+
+        $roi = $totalSpent > 0 ? (($totalRevenue - $totalSpent) / $totalSpent) * 100 : 0;
+
+        return new JsonResponse(
+            [
+                'campaignId' => $id,
+                'campaignName' => $campaign->getName(),
+                'budget' => round($totalSpent, 2),
+                'totalRevenue' => round($totalRevenue, 2),
+                'roiPercentage' => round($roi, 2),
+                'status' => $roi > 0 ? 'profitable' : 'loss',
+                'calculatedAt' => (new \DateTimeImmutable())->format('Y-m-d H:i:s')
+            ]
+        );
+    }
+}
